ef8b4beb0b97e7e8c21abeb1e919fee258a891d0
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-08-02 23:08:03 -0600

    concat indices, generating 1 random character only

--- script.js
+++ script.js
@@ -6,8 +6,11 @@
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-  var passwordlength = [""];
+  var PasswordLength = [""];
+  var randomIndex = numbers.concat(symbols, lowercase, uppercase);
+    console.log(randomIndex);
 
+// Write password to the #password input
   function writePassword() {
     var password = generatePassword() 
     var passwordText = document.querySelector("#password");
@@ -15,43 +18,34 @@
     passwordText.value = password;
   
   
-    if (uppercase && lowercase && numbers && symbols) {
+    //if (uppercase && lowercase && numbers && symbols) {
       // Loop the array elements and choose randomly
-    for (let i =0; i < length; i++) {
-    var randomIndex = Math.floor(Math.random() * length);
-    }
+    //for (let i =0; i < length; i++) {
+    //var randomIndex = Math.floor(Math.random() * length);
+    //}
+  //}
   }
-  }
-  // If no number is entered, prompt user to enter a number
   
   
   // Get references to the #generate element
   var generateBtn = document.querySelector("#generate");
-  
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
+
   
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
   
-    // Loop the array elements and choose randomly
-    for (let i =0; i < length; i++) {
-      var randomIndex = Math.floor(Math.random() * length);
-  
-    }
+    
 
     // Window Prompts
     function generatePassword() {
 
       var passwordLength = window.prompt("Select length between 8 and 128");
       console.log("password length " + passwordLength);
-
+      // If no number is entered, prompt user to enter a number  
+      if(passwordLength < 8 || passwordLength > 128) {
+          (window.alert("Please select between 8 and 128"));
+          return; 
+        } 
       confirmNumbers = confirm("Would you like to use numbers?") 
         if (confirmNumbers === true) {
           var computerChoice = numbers.index;
@@ -76,7 +70,7 @@
       if (confirmUppercase === true) {
         var computerChoice = uppercase.index;
       
-      console.log("uppercase have been selected " + uppercase);
+      console.log("Uppercase have been selected " + uppercase);
       } else {
         console.log("Uppercase will not be used");
       }
@@ -89,5 +83,14 @@
     } else {
       console.log("Lowercase will not be used");
     }
+    //var randomIndex = ("");
+// Loop the array elements and choose randomly
+      for (var i = 0; i <= length; i++) {
 
-  }
+        //Not Working.  Only pulling 1 random character, not up to the length
+        //Also not printing on screen, only printing to console.
+      passwordText = randomIndex[Math.floor(Math.random() * passwordLength)]
+        console.log("new password " + randomIndex[Math.floor(Math.random() * passwordLength)]);
+}
+console.log(randomIndex);
+}
