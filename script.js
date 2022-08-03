89a3947507775b0d488d335ad2f9b9193d8d74fd
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-08-02 21:49:47 -0600

    Functions start when generate password button is clicked

--- script.js
+++ script.js
@@ -8,87 +8,72 @@
 
   var passwordlength = [""];
 
-// Loop the array elements and choose randomly
-
-randomIndex = Math.floor(Math.random() * length);
+  function writePassword() {
+    var password = generatePassword() 
+    var passwordText = document.querySelector("#password");
   
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-var generateBtn = document.querySelector("#generate");
-// Window Prompts
-    var passwordLength = window.prompt("Select length between 8 and 128");
-    console.log("password length " + passwordLength);
-
-    window.confirm("Would you like to use numbers?") 
-    if (numbers === true) {
-    var computerChoice = numbers[index];
-    }
-   console.log("numbers have been selected " + numbers);
-
-
-   window.confirm("Would you like to use symbols?") 
-   if (symbols === true) {
-    var computerChoice = symbols[index];
-   }
-   console.log("symbols have been selected " + symbols);
-
-   window.confirm("Would you like to use uppercase?") 
-   if (uppercase === true) {
-    var computerChoice = uppercase[index];
-   }
-    console.log("uppercase have been selected " + uppercase);
-
-   window.confirm("Would you like to use lowercase?") 
-    if (lowercase === true) {
-    var computerChoice = lowercase[index];
-   }
-    console.log("lowercase have been selected " + lowercase);
-
-
+    passwordText.value = password;
+  
+  
     if (uppercase && lowercase && numbers && symbols) {
       // Loop the array elements and choose randomly
     for (let i =0; i < length; i++) {
     var randomIndex = Math.floor(Math.random() * length);
     }
-    }
+  }
+  }
+  // If no number is entered, prompt user to enter a number
   
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-
-  if (uppercase && lowercase && numbers && symbols) {
+  
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
     // Loop the array elements and choose randomly
-  for (let i =0; i < length; i++) {
-  var randomIndex = Math.floor(Math.random() * length);
-  }
-  }
-}
-// If no number is entered, prompt user to enter a number
+    for (let i =0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * length);
+  
+    }
+
+    // Window Prompts
+    function generatePassword() {
+
+      var passwordLength = window.prompt("Select length between 8 and 128");
+      console.log("password length " + passwordLength);
+
+      window.confirm("Would you like to use numbers?") 
+        if (numbers === true) {
+          var computerChoice = numbers[index];
+        }
+        console.log("numbers have been selected " + numbers);
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+      window.confirm("Would you like to use symbols?") 
+        if (symbols === true) {
+          var computerChoice = symbols[index];
+      }
+      console.log("symbols have been selected " + symbols);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+      window.confirm("Would you like to use uppercase?") 
+      if (uppercase === true) {
+        var computerChoice = uppercase[index];
+      }
+      console.log("uppercase have been selected " + uppercase);
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+      window.confirm("Would you like to use lowercase?") 
+      if (lowercase === true) {
+        var computerChoice = lowercase[index];
+      }
+      console.log("lowercase have been selected " + lowercase);
+    }
